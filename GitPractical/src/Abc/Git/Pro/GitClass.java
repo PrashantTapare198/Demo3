f840f02fa1fe1a3db9106026d9b79186327f5195
PrashantTapare198 <141405748+PrashantTapare198@users.noreply.github.com>
2023-08-05 12:48:43 +0530

    Update GitClass.java

--- GitPractical/src/Abc/Git/Pro/GitClass.java
+++ GitPractical/src/Abc/Git/Pro/GitClass.java
@@ -1,7 +1,12 @@
 package Abc.Git.Pro;
 
 public class GitClass {
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 public static void main(String[] args) {
 	System.out.println("Hello word");
+	
 }
 }
